10c329ca597f19dc4a096f14523e52d7b8b76387
emdean99 <deanethan3@gmail.com>
2022-03-14 16:20:01 -0400

    Create Weather Script

---
+++ weather_script.R
@@ -0,0 +1,12 @@
+# This script will download all current data for a specific tracker 
+# Location, detect the data range, download the data from the range 
+# From Visual Crossing and then export a set of graphs comparing the
+# Data range
+
+
+
+# First The Data will import the data into the working envirenemnt from 
+# the tracker
+
+tracker_name = "HBD"
+
